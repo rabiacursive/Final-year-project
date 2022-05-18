96e5d4e4f4e87ab719b8ac722af8c3213a42e271
Holliday <cursivemedia@MacBook-Pro-M1-Max.local>
2022-05-18 17:41:55 -0500

    shop stuff

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
             class="nav col-12 col-lg-auto ms-3 me-lg-auto mb-2 justify-content-center mb-md-0"
           >
             <li>
-              <a href="#" class="nav-link px-2 link-secondary">Shop</a>
+              <a href="#" class="nav-link px-2 link-secondary">Buy</a>
             </li>
             <li><a href="#" class="nav-link px-2 link-dark">Sell</a></li>
           </ul>
