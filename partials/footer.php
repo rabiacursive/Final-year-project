097d7967844dd53faad37cca04309439b31b2ef4
Holliday <cursivemedia@MacBook-Pro-M1-Max.local>
2022-05-19 12:05:08 -0500

    product stuff

---
+++ partials/footer.php
@@ -0,0 +1,82 @@
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add New Animal</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <form accept-charset="utf-8" action="https://www.beopari.pk/add-product-post" id="form_validate" method="post" name="form_validate" onkeypress="return event.keyCode != 13;">
+              <div class="form-group mb-3">
+                <input class="form-control form-input" maxlength="25" name="title" placeholder="Animal Name" required="" type="text">
+              </div>
+              <div class="form-group mb-3">
+                <textarea class="form-control form-input" maxlength="490" name="description" placeholder="Description" required=""></textarea>
+              </div>
+              <div class="form-group mb-3">
+                <input type='file' name='file' id='file' class='form-control' >
+              </div>
+              <div class="form-group form-group-category">
+                  <label class="control-label">Category</label> <select class="form-control custom-select m-0" id="categories" name="category_id[]" onchange="get_subcategories(this.value, 0);" required="">
+                    <option value="">
+                      Select Category
+                    </option>
+                    <option value="1">
+                      Goat
+                    </option>
+                    <option value="2">
+                      Cattle
+                    </option>
+                    <option value="3">
+                      Sheep
+                    </option>
+                    <option value="4">
+                      Chicken
+                    </option>
+                    <option value="5">
+                      Horse
+                    </option>
+                    <option value="6">
+                      Cat
+                    </option>
+                    <option value="7">
+                      Dog
+                    </option>
+                  </select>
+                  <div id="category_select_container"></div>
+                </div>
+
+</form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+      crossorigin="anonymous"
+    ></script>
---
+++ partials/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bootstrap demo</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossorigin="anonymous"
+    />
+        <link rel="stylesheet" href="/assets/css/product_info.css" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/shop.css" />
+  </head>
+  <body>
+<header class="p-3 mb-3 border-bottom">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+            <img src="assets/img/Beopari-logo.svg" style="width: 100px; display: inline-block; margin: auto">
+          </a>
+          <div class="dropdown ms-auto
+            text-end">
+            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="assets/img/rabia-profile.png" alt="mdo" class="rounded-circle" width="32" height="32">
+            </a>
+            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+              <li><a class="dropdown-item" href="#">Sign out</a></li>
+            </ul>
+          </div>
+        <ul style="margin: auto 10px;">
+            <li style="list-style: none">
+              <!-- Button trigger modal -->
+              <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="font-size: 27px;padding: 5px 15px;margin: auto;margin-right: 10px;line-height: 1;" class="btn btn-light border shadow">
+               +🐄
+              </button>
+            </li>
+          </ul></div>
+      </div>
+    </header>
